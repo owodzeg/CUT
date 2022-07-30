5bcff260e7b03e286f8010fed48c8eba3b9125f4
owocek <owocektv@gmail.com>
2022-07-31 00:13:06 +0200

    read data from buffer continously

---
+++ analyzer.c
@@ -0,0 +1,55 @@
+#include "analyzer.h"
+#include <unistd.h>
+#include <stdio.h>
+
+struct CoreData* old_core_data = NULL;
+struct CoreData* new_core_data = NULL;
+
+void* process_data(void* arg)
+{
+    sleep(2);
+
+    unsigned long numcores = 0;
+    numcores = get_num_cores();
+    
+    struct CoreData* tmp;
+
+    if(old_core_data == NULL)
+        old_core_data = malloc(sizeof(struct CoreData) * numcores);
+
+    if(new_core_data == NULL)
+        new_core_data = malloc(sizeof(struct CoreData) * numcores);
+
+    for(;;)
+    {
+        tmp = load_from_buffer();
+        
+        for(unsigned long i=0; i<numcores; i++)
+        {
+            new_core_data[i].core_id = tmp[i].core_id;
+            new_core_data[i].user = tmp[i].user;
+            new_core_data[i].nice = tmp[i].nice;
+            new_core_data[i].system = tmp[i].system;
+            new_core_data[i].idle = tmp[i].idle;
+            new_core_data[i].iowait = tmp[i].iowait;
+            new_core_data[i].irq = tmp[i].irq;
+            new_core_data[i].softirq = tmp[i].softirq;
+            new_core_data[i].steal = tmp[i].steal;
+        
+            // logic
+            printf("core %lu user %ld vs %ld\n", i+1, new_core_data[i].user, old_core_data[i].user);
+
+            old_core_data[i].core_id = new_core_data[i].core_id;
+            old_core_data[i].user = new_core_data[i].user;
+            old_core_data[i].nice = new_core_data[i].nice;
+            old_core_data[i].system = new_core_data[i].system;
+            old_core_data[i].idle = new_core_data[i].idle;
+            old_core_data[i].iowait = new_core_data[i].iowait;
+            old_core_data[i].irq = new_core_data[i].irq;
+            old_core_data[i].softirq = new_core_data[i].softirq;
+            old_core_data[i].steal = new_core_data[i].steal;
+        }
+
+        sleep(1);
+    }
+}
--- analyzer.h
+++ analyzer.h
@@ -1,1 +1,7 @@
 // module for processing the oncoming data from buffer
+#include "buffer.h"
+
+extern struct CoreData* old_core_data;
+extern struct CoreData* new_core_data;
+
+_Noreturn void* process_data(void*);
