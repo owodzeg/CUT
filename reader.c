a4357d09ac948d239e35180116222c30a71b1772
owocek <owocektv@gmail.com>
2022-07-30 11:08:21 +0200

    filter out cpu cores

--- reader.c
+++ reader.c
@@ -3,6 +3,7 @@
 #include "reader.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void read_proc()
 {
@@ -11,6 +12,8 @@ void read_proc()
 
     char cpu[255];
     long int user=0, nice=0, system=0, idle=0, iowait=0, irq=0, softirq=0, steal=0;
+
+    char head[3];
 
     fptr = fopen("/proc/stat", "r");
     
@@ -24,6 +27,9 @@ void read_proc()
     {
         //printf("%s", line);
         sscanf(line, "%s %ld %ld %ld %ld %ld %ld %ld %ld", cpu, &user, &nice, &system, &idle, &iowait, &irq, &softirq, &steal);
+        strncpy(head, cpu, 3);
+        
+        if(strcmp("cpu", head) == 0 && strlen(cpu) > 3)
         printf("cpu: %s, user: %ld, nice: %ld, system: %ld, idle: %ld, iowait: %ld, irq: %ld, softirq: %ld, steal: %ld\n", cpu, user, nice, system, idle, iowait, irq, softirq, steal);
     }
     
