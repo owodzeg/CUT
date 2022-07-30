37c7534d86ad26bd4823baeb54c5c6d3591dc958
owocek <owocektv@gmail.com>
2022-07-30 13:37:29 +0200

    run the reader function in a thread

--- main.c
+++ main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+#include <pthread.h>
 #include "reader.h"
 
 int main()
 {
+	pthread_t reader_thread;
+	int result = 0;
+
 	printf("Hello, %s!\n", "World");
 
-	read_proc();
+	result = pthread_create(&reader_thread, NULL, read_proc, NULL);
+	if(result != 0)
+	{
+		printf("Couldn't create thread\n");
+	}
+
+	pthread_join(reader_thread, NULL);
 
 	return 0;
 }
