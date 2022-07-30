dcd2d9a8eca9e5f3d1e7596790e18d2291ac48e3
owocek <owocektv@gmail.com>
2022-07-30 21:40:01 +0200

    create dynamic data buffer for core information

--- buffer.c
+++ buffer.c
@@ -1,11 +1,49 @@
 #include "buffer.h"
+#include <stdio.h>
 
-void add_to_queue(struct CoreData *core)
-{
-    //TODO: some queue logic here
-}
+int old_cores;
+int max_cores;
+struct CoreData *cores = NULL;
 
-struct CoreData read_from_queue(void* arg)
+void save_to_buffer(struct CoreData data)
 {
-    //TODO: some more queue logic here
+    int core_id = data.core_id;
+
+    old_cores = max_cores;
+
+    if(core_id+1 > max_cores)
+    max_cores = core_id+1;
+
+    if(cores == NULL)
+    {
+        printf("Initializing cores with size: %d\n", sizeof(struct CoreData));
+        cores = malloc(sizeof(struct CoreData));
+
+        if(!cores)
+        {
+            printf("Allocation failed\n");
+        }
+        else
+        {
+            printf("Success\n");
+        }
+    }
+    else if(old_cores != max_cores)
+    {
+        printf("Attempting to resize cores to size %d\n", sizeof(struct CoreData) * max_cores);
+        struct CoreData *cores_new = realloc(cores, sizeof(struct CoreData) * max_cores);
+
+        if(!cores_new)
+        {
+            printf("Allocation failed\n");
+        }
+        else
+        {
+            printf("Success\n");
+            cores = cores_new;
+        }
+    }
+
+    //after cores is resized, i can safely store more data
+    cores[core_id] = data;
 }
--- buffer.h
+++ buffer.h
@@ -1,6 +1,11 @@
 // module that'll be used for buffering oncoming data from reader, 
 // so it can be received by analyzer in a neat queue
 
+#ifndef BUFFER_H
+#define BUFFER_H
+
+#include <stdlib.h>
+
 // contain the necessary data of a single core
 // we differentiate cores so we can calculate per core % usage
 struct CoreData
@@ -16,5 +21,13 @@ struct CoreData
     long int steal;
 };
 
-void add_to_queue(struct CoreData *core);
-struct CoreData read_from_queue(void*);
+// we keep track of the old core count so we don't do expensive reallocs all the time
+extern int old_cores;
+extern int max_cores;
+
+// data storage
+extern struct CoreData *cores;
+
+void save_to_buffer(struct CoreData data);
+
+#endif
