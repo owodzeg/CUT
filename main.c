166bc89cd11b221396b0037c30306a0831754bca
owocek <owocektv@gmail.com>
2022-07-31 00:13:29 +0200

    deploy second thread for analyzer module

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <pthread.h>
 #include "reader.h"
+#include "analyzer.h"
 
 int main()
 {
 	pthread_t reader_thread;
+	pthread_t analyzer_thread;
 	int result = 0;
 
 	printf("Hello, %s!\n", "World");
@@ -15,7 +17,14 @@ int main()
 		printf("Couldn't create thread\n");
 	}
 
+	result = pthread_create(&analyzer_thread, NULL, process_data, NULL);
+	if(result != 0)
+	{
+		printf("Couldn't create thread\n");
+	}
+
 	pthread_join(reader_thread, NULL);
+	pthread_join(analyzer_thread, NULL);
 
 	return 0;
 }
