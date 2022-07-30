89d735bf0f45289bf79a9c02632fa2eb457dd6b3
owocek <owocektv@gmail.com>
2022-07-30 13:36:02 +0200

    wrap reader in an infinite loop

--- reader.c
+++ reader.c
@@ -1,41 +1,45 @@
-#define _GNU_SOURCE //define
-
 #include "reader.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-void read_proc()
+void* read_proc()
 {
-    FILE *fptr;
-    char line[1024];
-
-    char cpu[255];
-    long int user=0, nice=0, system=0, idle=0, iowait=0, irq=0, softirq=0, steal=0;
-
-    char head[3];
-    int core = 0;
-
-    fptr = fopen("/proc/stat", "r");
-    
-    if(fptr == NULL)
+    for(;;)
     {
-        //TODO: replace it with logger thread
-        printf("Could not open file.\n");
-    }
+        FILE *fptr;
+        char line[1024];
 
-    while(fgets(line, 1024, fptr) != NULL) 
-    {
-        //printf("%s", line);
-        sscanf(line, "%s %ld %ld %ld %ld %ld %ld %ld %ld", cpu, &user, &nice, &system, &idle, &iowait, &irq, &softirq, &steal);
-        strncpy(head, cpu, 3);
+        char cpu[255];
+        long int user=0, nice=0, system=0, idle=0, iowait=0, irq=0, softirq=0, steal=0;
+
+        char head[3];
+        int core = 0;
+
+        fptr = fopen("/proc/stat", "r");
         
-        if(strcmp("cpu", head) == 0 && strlen(cpu) > 3)
+        if(fptr == NULL)
         {
-            core = atoi(cpu+3);
-            printf("cpu: %d, user: %ld, nice: %ld, system: %ld, idle: %ld, iowait: %ld, irq: %ld, softirq: %ld, steal: %ld\n", core, user, nice, system, idle, iowait, irq, softirq, steal);
+            //TODO: replace it with logger thread
+            printf("Could not open file.\n");
         }
+
+        while(fgets(line, 1024, fptr) != NULL) 
+        {
+            //printf("%s", line);
+            sscanf(line, "%s %ld %ld %ld %ld %ld %ld %ld %ld", cpu, &user, &nice, &system, &idle, &iowait, &irq, &softirq, &steal);
+            strncpy(head, cpu, 3);
+            
+            if(strcmp("cpu", head) == 0 && strlen(cpu) > 3)
+            {
+                core = atoi(cpu+3);
+                printf("cpu: %d, user: %ld, nice: %ld, system: %ld, idle: %ld, iowait: %ld, irq: %ld, softirq: %ld, steal: %ld\n", core, user, nice, system, idle, iowait, irq, softirq, steal);
+            }
+        }
+        
+        fclose(fptr);
+
+        sleep(1);
     }
-    
-    fclose(fptr);
 }
