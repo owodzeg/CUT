4b1f861d1f5e992a482e318e813a6f7fb533509c
owocek <owocektv@gmail.com>
2022-07-30 19:30:59 +0200

    create a structure for cpu data

---
+++ buffer.c
@@ -0,0 +1,11 @@
+#include "buffer.h"
+
+void add_to_queue(struct CoreData *core)
+{
+    //TODO: some queue logic here
+}
+
+struct CoreData read_from_queue(void* arg)
+{
+    //TODO: some more queue logic here
+}
--- buffer.h
+++ buffer.h
@@ -1,2 +1,20 @@
 // module that'll be used for buffering oncoming data from reader, 
 // so it can be received by analyzer in a neat queue
+
+// contain the necessary data of a single core
+// we differentiate cores so we can calculate per core % usage
+struct CoreData
+{
+    int core_id;
+    long int user;
+    long int nice;
+    long int system;
+    long int idle;
+    long int iowait;
+    long int irq;
+    long int softirq;
+    long int steal;
+};
+
+void add_to_queue(struct CoreData *core);
+struct CoreData read_from_queue(void*);
