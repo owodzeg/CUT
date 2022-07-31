3af605ca6ba6286be6cb644d01c81afdf2621743
owocek <owocektv@gmail.com>
2022-07-31 16:03:10 +0200

    run printer thread

--- main.c
+++ main.c
@@ -2,16 +2,22 @@
 #include <pthread.h>
 #include "reader.h"
 #include "analyzer.h"
+#include "printer.h"
 
 int main()
 {
 	pthread_t reader_thread;
 	pthread_t analyzer_thread;
+	pthread_t printer_thread;
 	int result = 0;
 
-	printf("Hello, %s!\n", "World");
-
 	result = pthread_create(&reader_thread, NULL, read_proc, NULL);
+	if(result != 0)
+	{
+		printf("Couldn't create thread\n");
+	}
+
+	result = pthread_create(&printer_thread, NULL, print_data, NULL);
 	if(result != 0)
 	{
 		printf("Couldn't create thread\n");
@@ -23,6 +29,7 @@ int main()
 		printf("Couldn't create thread\n");
 	}
 
+	pthread_join(printer_thread, NULL);
 	pthread_join(reader_thread, NULL);
 	pthread_join(analyzer_thread, NULL);
 
