c1df5ab2c07ca9c487b704762e8be2b2ff369649
owocek <owocektv@gmail.com>
2022-07-31 14:08:28 +0200

    adjustable refresh rate and more accurate calculations

--- analyzer.c
+++ analyzer.c
@@ -25,9 +25,46 @@ void* process_data(void* arg)
 
     double percentage;
 
-    sleep(2);
+    int** percentagesStored; //we store many percentage data to get an average per second
 
+    int refresh_rate; //how many refreshes per second
+
+    double average;
+
+    int* averagesStored; //storage for final calculation
+
+    sleep(2); //wait to make sure we have an appropriate number of cores
+
+    refresh_rate = 10;
     numcores = get_num_cores();
+
+    percentagesStored = malloc(sizeof(int*) * numcores);
+    if(!percentagesStored)
+    {
+        printf("Unable to allocate memory for percentagesStored\n");
+    }
+    else
+    {
+        for(unsigned long i=0; i<numcores; i++)
+        {
+            percentagesStored[i] = malloc(sizeof(double) * refresh_rate);
+
+            if(!percentagesStored[i])
+            {
+                printf("Unable to allocate memory for percentagesStores[%lu]", i);
+            }
+        }
+    }
+
+    for(unsigned long c=0; c<numcores; c++)
+    {
+        for(int r=0; r<refresh_rate; r++)
+        {
+            percentagesStored[c][r] = 0.0;
+        }
+    }
+
+    averagesStored = malloc(sizeof(double) * numcores);
 
     if(old_core_data == NULL)
         old_core_data = malloc(sizeof(struct CoreData) * numcores);
@@ -66,9 +103,40 @@ void* process_data(void* arg)
             totalDiff = curTotal - prevTotal;
             idleDiff = curIdle - prevIdle;
 
-            percentage = (double)(totalDiff - idleDiff) / (double)totalDiff * 100.0;
+            //printf("totalDiff: %ld, idleDiff: %d", totalDiff, idleDiff);
 
-            printf("CPU core: %lu, usage: %f%%, totalDiff: %ld, idleDiff: %ld\n", i, percentage, totalDiff, idleDiff);
+            if(totalDiff > 0)
+            {
+                percentage = (double)(totalDiff - idleDiff) / (double)totalDiff * 100.0;
+            }
+            else
+            {
+                percentage = 0;
+            }
+
+            //printf("Percentage: %f", percentage);
+
+            for(int r=0; r<refresh_rate-1; r++)
+            {
+                percentagesStored[i][r] = percentagesStored[i][r+1];
+            }
+
+            percentagesStored[i][refresh_rate-1] = percentage;
+
+            average = 0.0;
+
+            for(int r=0; r<refresh_rate; r++)
+            {
+                average += percentagesStored[i][r];
+            }
+
+            //printf("Average value: %f", average);
+
+            average = average / (double)refresh_rate;
+
+            printf("CPU core: %lu, usage: %f%%\n", i, average);
+
+            averagesStored[i] = average;
 
             old_core_data[i].core_id = new_core_data[i].core_id;
             old_core_data[i].user = new_core_data[i].user;
@@ -81,6 +149,6 @@ void* process_data(void* arg)
             old_core_data[i].steal = new_core_data[i].steal;
         }
 
-        sleep(1);
+        usleep(1000000 / refresh_rate);
     }
 }
